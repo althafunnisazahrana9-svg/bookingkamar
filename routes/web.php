60e80081599d57d79a17fdc9aee378f5b39a064d
najwa <najwaaurel478@gmail.com>
2025-09-08 18:44:30 +0700

    membuat data admin

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ Auth::routes([
 
 
 Route::group([
-    'middleware' => ['auth']
+ 'middleware' => ['auth']
 ], function () {
-    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+ Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-    Route::get('/booking', [App\Http\Controllers\BookingController::class, 'index'])->name('booking.index');
-    Route::resource('/booking', App\Http\Controllers\BookingController::class);
-
-    Route::resource('/kamar', App\Http\Controllers\KamarController::class);
+Route::get('/booking', [App\Http\Controllers\BookingController::class, 'index'])->name('booking.index');
+Route::resource('/booking', App\Http\Controllers\BookingController::class);
+Route::resource('/admin', App\Http\Controllers\AdminController::class);
+Route::resource('/kamar', App\Http\Controllers\KamarController::class);
 
 });
