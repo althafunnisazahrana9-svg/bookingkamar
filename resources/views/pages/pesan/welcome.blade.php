1fbc5957d49b14bf0b8aba72aad5843edf057986
chika <fathonahp7@gmail.com>
2025-09-19 10:18:19 +0700

    edit warna

--- resources/views/pages/pesan/welcome.blade.php
+++ resources/views/pages/pesan/welcome.blade.php
@@ -9,10 +9,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
-<body class="bg-success">
+<body style="background-color: #D2B48C;">
 
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #D2B48C;">
         <div class="container">
             <a class="navbar-brand fw-bold" href="#">LOGO</a>
             <form class="d-flex ms-auto me-4">
@@ -28,7 +28,7 @@
             <!-- Left -->
             <div class="col-md-6">
                 <div class="text-warning fs-5">★★★★★</div>
-                <h1 class="fw-bold display-4 text-success">HOTEL <br> AETHERIA</h1>
+                <h1 class="fw-bold display-4" style="color: #A0522D;">HOTEL <br> AETHERIA</h1>
                 <p class="text-muted">
                     Selamat datang di Hotel Aetheria – tempat di mana kenyamanan, kemewahan,
                     dan ketenangan berpadu sempurna. Nikmati pengalaman menginap yang tak terlupakan bersama kami.
