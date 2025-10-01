16d9ff8bc59ef32b03ce62d48c6f6e1e2bf3c3f6
chika <fathonahp7@gmail.com>
2025-10-01 08:30:46 +0700

    membuat postingan master suit

--- resources/views/pages/news/master.blade.php
+++ resources/views/pages/news/master.blade.php
@@ -11,7 +11,7 @@
         <!-- Section 1: Gambar di kiri, teks di kanan -->
         <div class="row align-items-center mb-5">
             <div class="col-md-6">
-                <img src="{{ asset('images/master-suite-main.jpg') }}" class="img-fluid rounded" alt="Master Suite HOTEL">
+                <img src="{{ asset('images/master1.jpg') }}" class="img-fluid rounded" alt="Master Suite HOTEL">
             </div>
             <div class="col-md-6">
                 <h3>Kenyamanan Mewah yang Lebih Tinggi</h3>
@@ -27,8 +27,7 @@
         <!-- Section 2: Gambar di kanan, teks di kiri -->
         <div class="row align-items-center mb-5">
             <div class="col-md-6 order-md-2">
-                <img src="{{ asset('images/master-suite-interior.jpg') }}" class="img-fluid rounded"
-                    alt="Interior Master Suite">
+                <img src="{{ asset('images/master2.jpg') }}" class="img-fluid rounded" alt="Interior Master Suite">
             </div>
             <div class="col-md-6 order-md-1">
                 <h3>Fasilitas Modern & Premium</h3>
@@ -43,8 +42,7 @@
         <!-- Section 3: Gambar di kiri, teks di kanan -->
         <div class="row align-items-center mb-5">
             <div class="col-md-6">
-                <img src="{{ asset('images/master-suite-view.jpg') }}" class="img-fluid rounded"
-                    alt="Pemandangan dari Master Suite">
+                <img src="{{ asset('images/master3.jpg') }}" class="img-fluid rounded" alt="Pemandangan dari Master Suite">
             </div>
             <div class="col-md-6">
                 <h3>Pemandangan Eksklusif</h3>
@@ -56,10 +54,11 @@
             </div>
         </div>
 
-        <!-- Tombol kembali ke beranda -->
-        <div class="text-center mt-4">
-            <a href="{{ route('home') }}" class="btn btn-primary">Kembali ke Beranda</a>
+        <!-- Tombol Kembali -->
+        <div class="text-center mb-5">
+            <a href="{{ route('booking.news') }}" class="btn btn-secondary">
+                <i class="bi bi-arrow-left-circle me-2"></i> Kembali
+            </a>
         </div>
-
     </div>
 @endsection
