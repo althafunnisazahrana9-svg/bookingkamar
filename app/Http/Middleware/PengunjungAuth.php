c50ccff85d2da32ec0f4ef8b7a102950d17dc7d0
chika <fathonahp7@gmail.com>
2025-10-10 11:15:49 +0700

    menambahkan komen di pengunjungauth.php

--- app/Http/Middleware/PengunjungAuth.php
+++ app/Http/Middleware/PengunjungAuth.php
@@ -11,14 +11,19 @@ class PengunjungAuth
     /**
      * Handle an incoming request.
      *
+     * * Middleware ini digunakan untuk memastikan
+     * hanya pengunjung yang sudah login yang bisa mengakses halaman tertentu.
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next)
     {
+        // Cek apakah user dengan guard "pengunjung" sudah login atau belum
         if (! Auth::guard('pengunjung')->check()) {
+             // Jika belum login → arahkan ke halaman login pengunjung
             return redirect()->route('pengunjung.login');
         }
 
+        // Jika sudah login → lanjutkan request ke halaman yang dituju
         return $next($request);
     }
 }
